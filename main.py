16c6399bcc9755e6c4df786566ea6c4ce7fbc199
RisingEntropy <1724458359@qq.com>
2024-03-12 17:41:03 +0800

    upd

--- main.py
+++ main.py
@@ -12,21 +12,23 @@ DVSBasedExperiment.setup_Environment()
 cifar10dvs_arg = Argument()
 cifar10dvs_arg.device = "cuda:0"
 cifar10dvs_arg.datasetdir = "/Datasets/CIFAR10DVS"
-cifar10dvs_arg.T = 10
-cifar10dvs_arg.rank = 16
+cifar10dvs_arg.T = 12
+cifar10dvs_arg.rank = 8
 cifar10dvs_arg.b = 32
 cifar10dvs_arg.epoch = 200
 cifar10dvs_arg.num_class = 10
-experiment = DVSBasedExperiment("CIFAR10DVS_T14_r8", cifar10dvs_arg)
-net = VGG.VGG(num_class=cifar10dvs_arg.num_class,T=cifar10dvs_arg.T, rank=cifar10dvs_arg.rank, device=cifar10dvs_arg.device).to(cifar10dvs_arg.device)
+experiment = DVSBasedExperiment("CIFAR10DVS_T12_r8", cifar10dvs_arg)
+net = VGG.VGG_CIFAR(T=cifar10dvs_arg.T, rank=cifar10dvs_arg.rank, device=cifar10dvs_arg.device).to(cifar10dvs_arg.device)
 experiment.train(net, CIFAR10DVS, cifar10dvs_arg.num_class, train_set=None, validate_set=None)
 
 
 # For NCaltech-101
 # from spikingjelly.datasets.n_caltech101 import NCaltech101
+# net = VGG.VGG_CIFAR(T=cifar10dvs_arg.T, rank=cifar10dvs_arg.rank, device=cifar10dvs_arg.device).to(cifar10dvs_arg.device)
 # experiment.train(net, NCaltech101, 101, train_set=None, validate_set=None)
 
 # For datasets without a wrapping class, please use code below:
+# net = VGG.VGG_Gesture(T=cifar10dvs_arg.T, rank=cifar10dvs_arg.rank, device=cifar10dvs_arg.device).to(cifar10dvs_arg.device)
 # train_set = DVS128Gesture(cifar10dvs_arg.datasetdir, train=True, data_type='frame', split_by='number',
 #                               frames_number=cifar10dvs_arg.T)
 # test_set = DVS128Gesture(cifar10dvs_arg.datasetdir, train=False, data_type='frame', split_by='number',
